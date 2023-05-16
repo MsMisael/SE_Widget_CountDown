54da90f11f0f1baa987a8c9a84de783893a5ece3
Misael Silva de Souza <msmisael@outlook.com>
2023-05-16 19:01:16 -0300

    converterTempo
    Funcao pra formatar o tempo em uma array com os valores para formatacao transformando horas em minutos

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 
+
+function converterTempo(arr) {
+    let horas = parseInt(arr[0]);
+    let minutos = parseInt(arr[1]);
+    let segundos = parseInt(arr[2]);
+
+    if (horas < 0 || minutos < 0 || segundos < 0) {
+        return "Formato de tempo inválido: não pode haver valores negativos.";
+    }
+
+    let totalMinutos = (horas * 60) + minutos;
+    var minutosFormatados = totalMinutos < 10 ? "0" + Math.round(totalMinutos) : Math.round(totalMinutos);
+    var segundosFormatados = segundos < 10 ? "0" + Math.round(segundos) : Math.round(segundos);
+
+    return [minutosFormatados, segundosFormatados, false];
+}
+
 function diffInSeconds(date1, date2) {
     const date = date2 || new Date()
     const diff = date1.getTime() - date.getTime()
